12fdcf1a0bfc412b7e45d86e96784d6e4ba0ebd3
Dendi Sutiya <Dendi.10601009@student.polsub.ac.id>
2025-05-09 15:17:44 +0700

    uia

--- src/pages/user/Home.tsx
+++ src/pages/user/Home.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Button from "../../components/ui/Button";
+import { ContainerScroll } from "../../components/ui/container-scroll-animation";
+import ucingImage from "../../assets/ucing.png";
 
 const Home = () => {
     return (
+        <>
         <div className="flex flex-col items-center justify-center h-screen text-center p-4">
             <h1 className="text-4xl font-bold mb-4">Welcome to Our Website</h1>
             <p className="text-lg text-gray-600 mb-6">Explore our features and solutions to enhance your experience.</p>
@@ -16,6 +19,28 @@ const Home = () => {
                 </Link>
             </div>
         </div>
+        <ContainerScroll
+        titleComponent={
+            <>
+              <h1 className="text-4xl font-semibold text-black dark:text-white">
+                Unleash the power of <br />
+                <span className="text-4xl md:text-[6rem] font-bold mt-1 leading-none">
+                  Scroll Animations
+                </span>
+              </h1>
+            </>
+          }
+        >
+        <img
+          src={ucingImage}
+          alt="hero"
+          height={720}
+          width={1400}
+          className="mx-auto rounded-2xl object-cover h-full object-left-top"
+          draggable={false}
+        />
+        </ContainerScroll>
+        </>
     );
 };
 
