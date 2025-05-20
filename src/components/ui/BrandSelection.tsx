1d4027f11bba8172d2f030c253ad62fb13a25425
Dendi Sutiya <Dendi.10601009@student.polsub.ac.id>
2025-05-20 13:36:30 +0700

    doneeeeeeee

--- src/components/ui/BrandSelection.tsx
+++ src/components/ui/BrandSelection.tsx
@@ -15,23 +15,23 @@ const BrandSection: React.FC = () => {
         <div className="flex flex-wrap justify-between items-center gap-3 md:gap-2 max-w-full md:max-w-6xl w-full">
           {/* Brand Logos */}
           <span className="text-base md:text-3xl font-bold text-gray-800 leading-tight">
-            <p className="font-['Pacifico']">GreenCo</p>
+            <p className="">GreenCo</p>
           </span>
 
           <span className="text-base md:text-3xl font-bold text-gray-800 leading-tight">
-            <p className="font-['Abril_Fatface']">RoofTop</p>
+            <p className="">RoofTop</p>
           </span>
 
           <span className="text-base md:text-3xl font-bold text-gray-800 leading-tight">
-            <p className="font-['Great_Vibes']">EcoStay</p>
+            <p className="">EcoStay</p>
           </span>
 
           <span className="text-base md:text-3xl font-bold text-gray-800 leading-tight">
-            <p className="font-['Cardo']">UrbanZen</p>
+            <p className="">UrbanZen</p>
           </span>
 
           <span className="text-base md:text-3xl font-bold text-gray-800 leading-tight">
-            <p className="font-['Architects_Daughter']">SkyHigh</p>
+            <p className="">SkyHigh</p>
           </span>
         </div>
       </div>
